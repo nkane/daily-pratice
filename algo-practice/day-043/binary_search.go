549adab5b36680ed7cb3247486e8657197b8a01a
Nick Kane <nkanedev@gmail.com>
2024-08-12 10:17:36 -0400

    add: daily algorithms day 43

---
+++ algo-practice/day-043/binary_search.go
@@ -0,0 +1,24 @@
+package practice
+
+import "golang.org/x/exp/constraints"
+
+func BinarySearch[T constraints.Ordered](a []T, target T) (int, int) {
+	count := 0
+	lo := 0
+	hi := len(a) - 1
+	for lo <= hi {
+		count++
+		mid := (lo + hi) / 2
+		guess := a[mid]
+		if guess == target {
+			return mid, count
+		}
+		if guess > target {
+			hi = mid - 1
+		} else {
+			lo = mid + 1
+		}
+	}
+
+	return -1, count
+}
---
+++ algo-practice/day-043/tree.go
@@ -0,0 +1,187 @@
+package practice
+
+import (
+	"fmt"
+
+	"golang.org/x/exp/constraints"
+)
+
+type TreeNode[T any] struct {
+	Data   T
+	Left   *TreeNode[T]
+	Right  *TreeNode[T]
+	Parent *TreeNode[T]
+}
+
+func InorderTraversal[T any](node *TreeNode[T]) {
+	if node != nil {
+		InorderTraversal(node.Left)
+		fmt.Print(node.Data, " ")
+		InorderTraversal(node.Right)
+	}
+}
+
+func PreorderTraversal[T any](node *TreeNode[T]) {
+	if node != nil {
+		fmt.Print(node.Data, " ")
+		PreorderTraversal(node.Left)
+		PreorderTraversal(node.Right)
+	}
+}
+
+func PostorderTraversal[T any](node *TreeNode[T]) {
+	if node != nil {
+		PostorderTraversal(node.Left)
+		PostorderTraversal(node.Right)
+		fmt.Print(node.Data, " ")
+	}
+}
+
+func BFS[T any](node *TreeNode[T]) {
+	q := []*TreeNode[T]{
+		node,
+	}
+	for len(q) > 0 {
+		n := q[0]
+		q = q[1:]
+		fmt.Print(n.Data, " ")
+		if n.Left != nil {
+			q = append(q, n.Left)
+		}
+		if n.Right != nil {
+			q = append(q, n.Right)
+		}
+	}
+}
+
+func MaxDepth_DFS_Recursive[T any](node *TreeNode[T]) int {
+	if node == nil {
+		return 0
+	}
+	leftMax := MaxDepth_DFS_Recursive(node.Left) + 1
+	rightMax := MaxDepth_DFS_Recursive(node.Right) + 1
+	if leftMax > rightMax {
+		return leftMax
+	}
+	return rightMax
+}
+
+func MaxDepth_BFS[T any](node *TreeNode[T]) int {
+	depth := 0
+	q := []*TreeNode[T]{
+		node,
+	}
+	for len(q) > 0 {
+		l := len(q)
+		for i := 0; i < l; i++ {
+			n := q[0]
+			q = q[1:]
+			if n.Left != nil {
+				q = append(q, n.Left)
+			}
+			if n.Right != nil {
+				q = append(q, n.Right)
+			}
+		}
+		depth++
+	}
+	return depth
+}
+
+func MaxDepth_DFS_Preorder_Iterative[T any](node *TreeNode[T]) int {
+	depth := 0
+	type StackFrame struct {
+		Node  *TreeNode[T]
+		Depth int
+	}
+	s := []StackFrame{
+		{
+			Node:  node,
+			Depth: 1,
+		},
+	}
+	for len(s) > 0 {
+		sf := s[len(s)-1]
+		s = s[:len(s)-1]
+		if sf.Node != nil {
+			if sf.Depth > depth {
+				depth = sf.Depth
+			}
+			if sf.Node.Left != nil {
+				s = append(s, StackFrame{
+					Node:  sf.Node.Left,
+					Depth: sf.Depth + 1,
+				})
+			}
+			if sf.Node.Right != nil {
+				s = append(s, StackFrame{
+					Node:  sf.Node.Right,
+					Depth: sf.Depth + 1,
+				})
+			}
+		}
+	}
+	return depth
+}
+
+func GoodNodes[T constraints.Ordered](node *TreeNode[T]) int {
+	type Frame struct {
+		Node *TreeNode[T]
+		Max  T
+	}
+	goodNodes := 0
+	q := []Frame{
+		{
+			Node: node,
+			Max:  node.Data,
+		},
+	}
+	for len(q) > 0 {
+		n := q[0]
+		q = q[1:]
+		if n.Node != nil {
+			if n.Node.Data >= n.Max {
+				goodNodes++
+			}
+			if n.Node.Left != nil {
+				max := n.Max
+				if max < n.Node.Left.Data {
+					max = n.Node.Left.Data
+				}
+				q = append(q, Frame{
+					Node: n.Node.Left,
+					Max:  max,
+				})
+			}
+			if n.Node.Right != nil {
+				max := n.Max
+				if max < n.Node.Right.Data {
+					max = n.Node.Right.Data
+				}
+				q = append(q, Frame{
+					Node: n.Node.Right,
+					Max:  max,
+				})
+			}
+		}
+	}
+	return goodNodes
+}
+
+func GoodNodes_DFS[T constraints.Ordered](node *TreeNode[T]) int {
+	return DFS(node, node.Data)
+}
+
+func DFS[T constraints.Ordered](node *TreeNode[T], maxValue T) int {
+	if node == nil {
+		return 0
+	}
+	res := 0
+	if node.Data >= maxValue {
+		res = 1
+		maxValue = node.Data
+	}
+	res += DFS(node.Left, maxValue)
+	res += DFS(node.Right, maxValue)
+	return res
+}

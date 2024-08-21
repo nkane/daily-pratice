5218223332d69ec25764d7be96796d5bdea12f8f
Nick Kane <nkanedev@gmail.com>
2024-08-21 10:39:33 -0400

    update: daily algorithms template to include new binary search problem from leetcode

--- algo-practice/_template/binary_search.go
+++ algo-practice/_template/binary_search.go
@@ -5,3 +5,10 @@ import "golang.org/x/exp/constraints"
 func BinarySearch[T constraints.Ordered](a []T, target T) (int, int) {
 	return -1, -1
 }
+
+/*
+162: Find Peak Element
+*/
+func findPeakElement(nums []int) int {
+	return -1
+}
--- algo-practice/_template/binary_search_test.go
+++ algo-practice/_template/binary_search_test.go
@@ -26,3 +26,28 @@ func TestBinarySearch(t *testing.T) {
 		assert.Assert(t, test.ExpectedCount == count)
 	}
 }
+
+func TestFindPeakElement(t *testing.T) {
+	tests := []struct {
+		Name   string
+		Nums   []int
+		Output int
+	}{
+		{
+			Name:   "Example 1",
+			Nums:   []int{1, 2, 3, 1},
+			Output: 2,
+		},
+		{
+			Name:   "Example 2",
+			Nums:   []int{1, 2, 1, 3, 5, 6, 4},
+			Output: 5,
+		},
+	}
+	for _, test := range tests {
+		t.Run(test.Name, func(t *testing.T) {
+			got := findPeakElement(test.Nums)
+			assert.DeepEqual(t, test.Output, got)
+		})
+	}
+}

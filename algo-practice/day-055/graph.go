9b43ca35d7024e664ef3e960e25f8fb00ab205ff
Nick Kane <nkanedev@gmail.com>
2024-08-24 10:28:56 -0400

    add: daily algorithms day 55

---
+++ algo-practice/day-055/graph.go
@@ -0,0 +1,22 @@
+package practice
+
+import "fmt"
+
+type Graph[K comparable, T any] struct {
+	Visited       map[K]struct{}
+	AdjacencyList map[K][]K
+}
+
+func (g *Graph[K, T]) AddEdge(v K, w K) {
+	g.AdjacencyList[v] = append(g.AdjacencyList[v], w)
+}
+
+func (g *Graph[K, T]) DFS(v K) {
+	g.Visited[v] = struct{}{}
+	fmt.Print(v, " ")
+	for _, edge := range g.AdjacencyList[v] {
+		if _, visited := g.Visited[edge]; !visited {
+			g.DFS(edge)
+		}
+	}
+}
